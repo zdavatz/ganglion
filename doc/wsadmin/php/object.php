797a575d0266edd9fef409f26b14866fe32c38b6
b123400 <i@b123400.net>
2021-11-08 18:10:22 +0900

    Re-implement delete functions deleted in #19

--- doc/wsadmin/php/object.php
+++ doc/wsadmin/php/object.php
@@ -6,11 +6,11 @@ function SendFormButtons($buttons)
 	echo "<input type='submit' name='send' value='Speichern'>\n";
 	echo "<input type='reset' name='cancel' value='Zur&uuml;cksetzen'>\n";
 	if (in_array("delete", $buttons))
-		echo "<input type='button' name='delete' value='Eintrag l&ouml;schen' onclick='entryDelete(titel)'>\n";
+		echo "<input type='button' name='delete' value='Eintrag l&ouml;schen' onclick='entryDelete()'>\n";
 	if (in_array("delete_pdf", $buttons))
-		echo "<input type='button' name='delete' value='Pdf l&ouml;schen' onclick='pdfDelete(pdfname)'>\n";
+		echo "<input type='button' name='delete' value='Pdf l&ouml;schen' onclick='pdfDelete()'>\n";
 	if (in_array("delete_audiofile", $buttons))
-		echo "<input type='button' name='delete' value='Audiofile l&ouml;schen' onclick='audiofileDelete(audiofilename)'>\n";
+		echo "<input type='button' name='delete' value='Audiofile l&ouml;schen' onclick='audiofileDelete()'>\n";
 }
 function hiddenfields($all){
 	if (isset($new) && $new == "true")
@@ -20,3 +20,20 @@ function hiddenfields($all){
 }
 $bereich_array = array("Familie","Arbeit","Gesundheit","Erziehung","zur Person"); 
 ?>
+<script>
+function entryDelete() {
+	if (confirm('Wollen Sie den Eintrag wirklich löschen?')) {
+		document.getElementsByName('entrydelete')[0].submit();
+	}
+}
+function pdfDelete() {
+	if (confirm('Wollen Sie diese Pdf Datei wirklich löschen?')) {
+		document.getElementsByName('pdfdelete')[0].submit();
+	}
+}
+function audiofileDelete() {
+	if (confirm('Wollen Sie diese Audio Datei wirklich löschen?')) {
+		document.getElementsByName('audiofiledelete')[0].submit();
+	}
+}
+</script>
