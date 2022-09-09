9ef1248394cff157fd8c8c1d5c9ed3cda6a10552
zdavatz@gmail.com <zdavatz@gmail.com>
2022-09-09 16:54:04 +0200

    update Video Link

--- doc/index.php
+++ doc/index.php
@@ -115,7 +115,7 @@ Fax: 056 200 08 18<br>
 </tr>
 <td class="TDbold-big" colspan="3"><a href=https://adhs.expert/wp-content/uploads/2022/01/PSYCHOLOGINNEN-Praxis-Zürich_Winterthurerstrasse-52.pdf target=_blank>Psychologinnen Zürich</a></td>
 </tr>
-<td class="TDbold-big" colspan="3"><a href=https://psychiatriewohin.ch/ target=_blank>Grossanlass "Psychiatrie wohin?" zum 80-jährigen Geburtstag von Dr.med. Ursula Davatz</a></td>
+<td class="TDbold-big" colspan="3"><a href=https://youtube.com/playlist?list=PLx8ORGwBUsmAeZA5lJs95ngsP57Nr3Tcj target=_blank>Grossanlass "Psychiatrie wohin?" zum 80-jährigen Geburtstag von Dr.med. Ursula Davatz</a></td>
 </table>
 </td>
 </tr>
