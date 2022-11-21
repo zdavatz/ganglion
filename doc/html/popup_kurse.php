5e60a5a62b515da6a03a303d3adc410229414f9b
zdavatz@gmail.com <zdavatz@gmail.com>
2022-11-21 15:13:26 +0100

    add a Google Form

--- doc/html/popup_kurse.php
+++ doc/html/popup_kurse.php
@@ -43,80 +43,24 @@ $url = $_SERVER["PHP_SELF"];
   	$values = mysqli_fetch_assoc($result);
 		?>
 			<table class="bordertable">
-			<tr><td class='TDbold-big'>Ganglion Anmeldung</td></tr>
+			<tr><td class='TDbold-big'>Anmeldung</td></tr>
 			<tr>
 			<?php echo "<td colspan='2' class='TDbold'>".stripslashes(trim(urldecode ($values["titel_kurse"])))."</td>"; ?>
 			</tr>
-			<tr><td class='TDbold'>Adresse Kursteilnehmer</td></tr>
-			<FORM METHOD="POST" ACTION="/html/popup_gesendet.php">
-<input type="hidden" name="subject" value="Anmeldung: <?php echo stripslashes(urldecode ($values["titel_kurse"]));?>">
-<input type="hidden" name="kurs_id" value="<?php echo $id;?>">
-			
-			</tr>
-					<tr>
-						<td>Anrede</td>
-						<td><INPUT TYPE="text" Name="title" size="50" value="<?php echo @$user_input["title"];?>"></td>
-					</tr>
-					<tr>
-						<td>Name</td>
-						<td><INPUT TYPE="text" Name="name" size="50" value="<?php echo	@$user_input["name"];?>"></td>
-					</tr>	
-					<tr>
-						<td>Vorname</td>
-						<td><INPUT TYPE="text" Name="firstname" size="50" value="<?php echo @$user_input["firstname"];?>"></td>
-					</tr>
-					<tr>
-						<td>Firma</td>
-						<td><INPUT TYPE="text" Name="company" size="50" value="<?php echo @$user_input["company"];?>"></td>
-					</tr>
-					<tr>
-						<td>Adresse</td>
-						<td><INPUT TYPE="text" Name="address" size="50" value="<?php echo @$user_input["address"];?>"></td>
-					</tr>
-					<tr>
-						<td>PLZ / Ort</td>
-						<td><INPUT TYPE="text" Name="plz_location" size="50" value="<?php echo	@$user_input["plz_location"];?>"></td>
-					</tr>
-					<tr>
-						<td>Telefon Privat</td>
-						<td><INPUT TYPE="text" Name="phone_privat" size="50" value="<?php echo @$user_input["phone_privat"];?>"></td>
-					</tr>
-					<tr>
-						<td>Telefon Gesch&auml;ft</td>
-						<td><INPUT TYPE="text" Name="phone_geschaeft" size="50" value="<?php echo @$user_input["phone_geschaeft"];?>"></td>
-					</tr>
-					<tr>
-						<td>Telefon Mobile</td>
-						<td><INPUT TYPE="text" Name="phone_mobile" size="50" value="<?php echo @$user_input["phone_mobile"];?>"></td>
-					</tr>
-					<tr>
-					<tr>
-						<td>Email-Adresse</td>
-						<td><INPUT TYPE="text" Name="email" size="50"  value="<?php echo @$user_input["email"];?>"></td>
-					</tr>
-						<tr>
-						<td><b>Anmeldung</b> per <b>Email</b></td>
-						<td>
-							<INPUT type="submit" value="Abschicken">
-							<input type="reset" value="Zur&uuml;cksetzen">
-						</td>
-					</tr>
+			<tr><td class='TDbold'>Anmeldeformular<a href="https://forms.gle/q6Da1cFhF5jvQHA89">hier klicken</a></td></tr>
 			</table>
 			</form>
 			<tr><td>&nbsp;</td></tr>
 <table class="bordertable">
 <tr>
-<td class="tabltxt-l"><b>Anmeldung</b> per Email an <b><a href="mailto:sekretariat@ganglion.ch">sekretariat@ganglion.ch</a></b></td>
-<td align="left">
-<button onClick='javascript:window.print()'>Drucken</button>
-</td>
+<td class="tabltxt-l"><b>Fragen</b> per Email an <b><a href="mailto:sekretariat@ganglion.ch">sekretariat@ganglion.ch</a></b></td>
 	</tr>
 </table>
 			<tr><td>&nbsp;</td></tr>
 			<?php
 			echo "<table class='borderTABLE'>";
 			echo "<tr>";
-			echo "<tr><td colspan='2' class='TDbold-big'>Ganglion Kursinhalt</td></tr>";
+			echo "<tr><td colspan='2' class='TDbold-big'>Inhalt der Veranstaltung</td></tr>";
 			echo "<td colspan='2' class='TDbold-big'>".stripslashes(trim(urldecode ($values["titel_kurse"])))."</td>";
 			echo "<td>&nbsp;</td>";
 			echo "</tr>";
